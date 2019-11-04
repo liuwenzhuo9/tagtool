59004068768d4d254da5368e843442b574979eac
liuwenzhuo <1299524871@qq.com>
2019-11-04 19:11:02 +0800

    部署版本1.0

--- src/components/tagResult.vue
+++ src/components/tagResult.vue
@@ -1,6 +1,7 @@
 <template>
     <div class='output'>从第&nbsp;<el-input v-model="begin" size="mini"></el-input>&nbsp;句到第&nbsp;<el-input v-model="end" size="mini"></el-input>&nbsp;句 
-    <el-button type="success"  @click="outTxt(begin,end)" v-if="textarea ==''">导出</el-button>
+    <el-button type="success"  @click="outTxt(begin,end,1)" v-if="textarea ==''">大实体靠前导出</el-button>
+    <el-button type="success"  @click="outTxt(begin,end,0)" v-if="textarea ==''">小实体靠前导出</el-button>
     <el-input type="textarea" :autosize="{ minRows: 6, maxRows: 20}"  v-model="textarea" v-if="showInput"></el-input>
     <el-button type="success" @click="saveTxt('test.txt',textarea)" v-if="textarea!=''">确认保存为txt</el-button>
     <el-button type="warning" @click="cancel()" v-if="textarea!=''">取消</el-button>
@@ -22,7 +23,7 @@ export default {
         }
     },
     methods:{
-        async outTxt(begin,end){
+        async outTxt(begin,end,type){
             const sentencesInfo = await findSentenceFromOffset({offset:begin-1,count:end-begin+1});
             this.allSentences = [];
             await Promise.all(sentencesInfo.data.map(async (item)=>{
@@ -35,7 +36,7 @@ export default {
                     this.startIndex.push(item.start_index);
                     this.endIndex.push(item.end_index);
                 })
-                this.addBIO(splitSentence,this.startIndex,this.endIndex);
+                this.addBIO(splitSentence,this.startIndex,this.endIndex,type);
                 this.allSentences = this.allSentences.concat(splitSentence);
             }))
             this.showInput = true;
@@ -45,34 +46,33 @@ export default {
             }
         },
 
-        addBIO(all,start,end){
-            window.console.log(all)
-            window.console.log(start)
-            window.console.log(end)
+        addBIO(all,start,end,type){
             let lengthOfSentence = all.length;
-            var minStart = 0, indexOfMin = 0, minEnd = 0;
+            var minStart = 0, minEnd = 0;
             for(let i = 1; start.length != 0;i++){//判断该句子实体展示需要多少列
             window.console.log(start)
                 var lineStartIndex =[];//存储当列展现的实体位置
                 var lineEndIndex =[];
                 minStart = Math.min.apply(null,start);
-                indexOfMin = start.indexOf(minStart);
-                minEnd = end[indexOfMin];
+                if(type == 0){
+                    minEnd = this.findShortestEndIndex(minStart,start,end)//该起始位置的最短实体
+                }else if(type == 1){
+                    minEnd = this.findLongestEndIndex(minStart,start,end)
+                }
                 lineStartIndex.push(minStart);
                 lineEndIndex.push(minEnd);//记录到当列存储数组中,元素为实体在句子中的位置
-                start.splice(indexOfMin,1);
-                end.splice(indexOfMin,1);//删除已记录到列的位置信息
                 //lastEndIndex为当列目前存在的、最后一个实体的位置
                 // nextStart为得到的下一个实体位置
                 for (var lastEndIndex = minEnd; lastEndIndex < lengthOfSentence; ){
                     var nextStart = this.findMinStartAfterEndIndex(lastEndIndex,start);
                     if(nextStart != -1){
-                        var indexOfnextStart = start.indexOf(nextStart);
-                        var nextEnd = end[indexOfnextStart]
+                        if(type == 0){
+                            var nextEnd = this.findShortestEndIndex(nextStart,start,end)
+                        }else if(type == 1){
+                            nextEnd = this.findLongestEndIndex(nextStart,start,end)
+                        }
                         lineStartIndex.push(nextStart);
                         lineEndIndex.push(nextEnd);
-                        start.splice(indexOfnextStart,1);
-                        end.splice(indexOfnextStart,1);
                         lastEndIndex = nextEnd;
                     }else{
                         break;
@@ -122,6 +122,36 @@ export default {
                 return -1;
             }
             
+        },
+        findShortestEndIndex(startNum,start,end){//a是实体起始位置，需要找到从该点开始的最短实体
+            var endNum = [];//存放startNum对应的实体结束位置
+            var indexOfEndNUm = [];//startNum对应的实体结束位置在end中的位置，便于从start和end中删除对应元素
+            for(let index = 0; start.indexOf(startNum,index) != -1;){//找到startnum在起始位置序列中的位置，在结束位置序列的对应位置找到endnum
+                index = start.indexOf(startNum,index);
+                indexOfEndNUm.push(index);
+                endNum.push(end[index]);
+                index = index + 1;
+            }
+            var shortestEnd = Math.min.apply(null,endNum);
+            var indexOfShortestEnd = indexOfEndNUm[endNum.indexOf(shortestEnd)];
+            start.splice(indexOfShortestEnd,1);
+            end.splice(indexOfShortestEnd,1);
+            return shortestEnd;
+        },
+        findLongestEndIndex(startNum,start,end){//a是实体起始位置，需要找到从该点开始的最长实体
+            var endNum = [];//存放startNum对应的实体结束位置
+            var indexOfEndNUm = [];//startNum对应的实体结束位置在end中的位置，便于从start和end中删除对应元素
+            for(let index = 0; start.indexOf(startNum,index) != -1;){//找到startnum在起始位置序列中的位置，在结束位置序列的对应位置找到endnum
+                index = start.indexOf(startNum,index);
+                indexOfEndNUm.push(index);
+                endNum.push(end[index]);
+                index = index + 1;
+            }
+            var longestEnd = Math.max.apply(null,endNum);
+            var indexOfLongestEnd = indexOfEndNUm[endNum.indexOf(longestEnd)];
+            start.splice(indexOfLongestEnd,1);
+            end.splice(indexOfLongestEnd,1);
+            return longestEnd;
         }
 
     }
